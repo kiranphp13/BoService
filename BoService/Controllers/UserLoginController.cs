ff557664a2b352e48fa8433b902a61248a5afec0
kiranphp13 <kiranphp13@gmail.com>
2021-02-25 11:13:22 +0530

    Initial commit

---
+++ BoService/Controllers/UserLoginController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using BoService.Models;
+
+namespace BoService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserLoginController : ControllerBase
+    {
+        private IConfiguration _config;
+        public BoAppDB Db { get; }
+        public UserLoginController(BoAppDB db, IConfiguration config)
+        {
+            Db = db;
+            _config = config;
+        }
+        // POST api/blog
+        [HttpPost]
+        public Dictionary<string, object> Post([FromBody] BoService.Models.Users value)
+        {
+            Dictionary<string, object> response = new Dictionary<string, object>();
+            try
+            {
+                Db.Connection.Open();
+                bool bIsValidUser = false;
+                string username = value.UserName;
+                string password = value.Password;
+                BoService.Models.Users objUsers = new BoService.Models.Users(Db);
+               
+                Users lstobjUsers = objUsers.GetUsersList(value.UserName, value.Password).FirstOrDefault();
+
+                //bIsValidUser = objUsers.IsUserRegistered();
+                if(lstobjUsers!=null)
+                {
+                    var jwt = new BoService.Authentication.JwtService(_config);
+                    var token = jwt.GenerateSecurityToken(value.UserName);
+
+                    if(lstobjUsers.Role.Contains("Member"))
+                    {
+                        response.Add("status", "success");
+                        response.Add("User Status", "Valid User Credential...");
+                        response.Add("User Token", token);
+
+                    }
+
+
+                }
+                else
+                {
+                    response.Add("status", "Error");
+                    response.Add("message", "Invalid User Credentials...");
+                }
+            }
+            catch(Exception Ex)
+            {
+                response.Add("status", "Error");
+                response.Add("message", Ex.Message);
+            }
+            return response;
+        }
+     }
+}
---
+++ BoService/Models/Users.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoService.Models
+{
+    public class Users
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+
+        public string UserName { get; set; }
+       
+        public string Role { get; set; }
+
+        internal BoAppDB Db { get; set; }
+
+        public Users()
+        {
+            Name = string.Empty;
+            Password = string.Empty;
+            Email = string.Empty;
+            Phone = string.Empty;
+
+        }
+
+        internal Users(BoAppDB db)
+        {
+            Db = db;
+            Name = string.Empty;
+            Password = string.Empty;
+            Email = string.Empty;
+            Phone = string.Empty;
+        }
+
+        public List<Users> GetUsersList(string strUserName, string strUserOassword)
+        {
+            List<Users> returnList = new List<Users>();
+            try
+            {
+                using var cmd = Db.Connection.CreateCommand();
+                string commandText = "SELECT * FROM edc.bousers where name = " + "'" + strUserName + "'" + "and password = " + "'" + strUserOassword + "'";
+                cmd.CommandText = commandText;
+                returnList = ReadAllAsync(cmd.ExecuteReader());
+            }
+            catch (Exception Ex)
+            {
+                returnList = null;
+            }
+            return returnList;
+        }
+
+        private List<Users> ReadAllAsync(System.Data.Common.DbDataReader reader)
+        {
+            List<Users> posts = new List<Users>();
+            try
+            {
+                using (reader)
+                {
+                    while (reader.Read())
+                    {
+                        var post = new Users(Db)
+                        {
+                            Name = reader.GetString(0),
+                            Password = reader.GetString(1),
+                            Email = reader.GetString(2),
+                            Phone = reader.GetString(3),
+                            Role = reader.GetString(4),
+                        };
+                        posts.Add(post);
+                    }
+                }
+            }
+            catch(Exception Ex)
+            {
+                posts = null;
+            }
+            return posts;
+        }
+
+        public bool IsUserRegistered()
+        {
+            bool bIsUserRegistered = false;
+            try
+            {
+                List<Users> result = GetUsersList(Name, Password);
+                if (result != null && result.Count > 0)
+                {
+                    bIsUserRegistered = true;
+
+                }
+                else
+                {
+                    bIsUserRegistered = false;
+                }
+            }
+            catch(Exception Ex)
+            {
+                bIsUserRegistered = false;
+            }
+            return bIsUserRegistered;
+        }
+    }
+}
